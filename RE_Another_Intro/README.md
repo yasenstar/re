5160bb97ee5905d030c55967369931a926ba85a9
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-04-23 21:39:53 +0800

    initial

---
+++ RE_Another_Intro/README.md
@@ -0,0 +1,5 @@
+# 正则指引
+
+## 主要章节内容
+
+![book_toc](img/Regular_Expression_Intro.png)
---
+++ RE_Another_Intro/Regular_Expression_Intro.mm
@@ -0,0 +1,109 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Regular Expression&#xa;正则指引" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1745415253676"><hook NAME="MapStyle" background="#f5f5dcff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sepia_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="第一部分" POSITION="bottom_or_right" ID="ID_1634200318" CREATED="1745415323805" MODIFIED="1745415328081">
+<node TEXT="第一章 字符组" ID="ID_1828395059" CREATED="1745415328087" MODIFIED="1745415338132"/>
+<node TEXT="第二章 量词" ID="ID_983374968" CREATED="1745415338393" MODIFIED="1745415344842"/>
+<node TEXT="第三章 括号" ID="ID_1232893036" CREATED="1745415345030" MODIFIED="1745415355058"/>
+<node TEXT="第四章 断言" ID="ID_1171113768" CREATED="1745415359039" MODIFIED="1745415364779"/>
+<node TEXT="第五章 匹配模式" ID="ID_1029073360" CREATED="1745415364943" MODIFIED="1745415370032"/>
+<node TEXT="第六章 其他" ID="ID_1272221379" CREATED="1745415370176" MODIFIED="1745415373569"/>
+</node>
+<node TEXT="第二部分" POSITION="bottom_or_right" ID="ID_1129696506" CREATED="1745415374309" MODIFIED="1745415376447">
+<node TEXT="第七章 Unicode" ID="ID_693643291" CREATED="1745415376459" MODIFIED="1745415381458"/>
+<node TEXT="第八章 匹配原理" ID="ID_752023256" CREATED="1745415381886" MODIFIED="1745415388546"/>
+<node TEXT="第九章 常见问题的解决思路" ID="ID_1532897069" CREATED="1745415389638" MODIFIED="1745415399015"/>
+</node>
+<node TEXT="第三部分" POSITION="bottom_or_right" ID="ID_1063030749" CREATED="1745415400343" MODIFIED="1745415405573">
+<node TEXT="第十章 .NET" ID="ID_324802389" CREATED="1745415405578" MODIFIED="1745415412152"/>
+<node TEXT="第十一章 Java" ID="ID_1152018807" CREATED="1745415416486" MODIFIED="1745415425449"/>
+<node TEXT="第十二章 JavaScript" ID="ID_839748308" CREATED="1745415425647" MODIFIED="1745415433470"/>
+<node TEXT="第十三章 PHP" ID="ID_1151588892" CREATED="1745415433637" MODIFIED="1745415439079"/>
+<node TEXT="第十四章 Python" ID="ID_902771365" CREATED="1745415439231" MODIFIED="1745415445033"/>
+<node TEXT="第十五章 Ruby" ID="ID_316753031" CREATED="1745415445198" MODIFIED="1745415450151"/>
+<node TEXT="第十六章 Objective-C" ID="ID_18141700" CREATED="1745415450309" MODIFIED="1745415457056"/>
+<node TEXT="第十七章 Golang" ID="ID_1804611588" CREATED="1745415457204" MODIFIED="1745415462880"/>
+<node TEXT="第十八章 Linux/UNIX" ID="ID_730981639" CREATED="1745415463047" MODIFIED="1745415470183"/>
+</node>
+</node>
+</map>
